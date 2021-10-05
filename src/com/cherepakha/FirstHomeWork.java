20a6a93eb66bfa2ac8a99cc16fbc35ce3b3d823d
”Ivan <mddfsaff@gmail.com>
2021-10-05 09:25:16 +0300

    first homework

---
+++ src/com/cherepakha/FirstHomeWork.java
@@ -0,0 +1,135 @@
+package com.cherepakha;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class FirstHomeWork {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+           Задание №2: Вывести ряд чисел от 1 до 100 через пробел, с изменениями:
+             - если число кратно 3 то вместо него вывести Hello
+             - если число кратно 5 то вместо него вывести World
+             - если число кратно и 3 и 5 то вместо него вывести HelloWorld
+        */
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.print("Hello ");
+            } else if (i % 3 != 0 && i % 5 == 0) {
+                System.out.print("World ");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("HelloWorld ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        /*
+          Задание №3: Заполнить массив из 10 элементов случайными целыми числами от 10(включительно) до 20(включительно).
+          Вывести среднее арифметическое.
+         */
+
+        int[] arrayOfIntegers = new int[10];
+        for (int i = 0; i < arrayOfIntegers.length; i++) {
+            arrayOfIntegers[i] = (int) (10 + (Math.random() * (21 - 10)));
+        }
+
+
+        int sum = 0;
+        for (int i = 0; i < arrayOfIntegers.length; i++) {
+            sum += arrayOfIntegers[i];
+        }
+
+        double arithmeticMean = sum / 10;
+        System.out.println(arithmeticMean);
+
+        System.out.println();
+
+
+        /*
+          Задание №4: Написать метод который вернет String в которой будут в одну строку числа от 1 до 30.
+          Каждое число при этом взять в круглые скобки ()
+         */
+
+        String stringOfNumbers = getString();
+        System.out.println(stringOfNumbers);
+        System.out.println();
+
+        /*
+           Задание №5: Написать метод который убирает лишние пробелы в строке и возвращает строку.
+           Убрать начальные, конечные и между словами должно быть не больше 1 пробела.
+         */
+
+        String stringWithSpaces = " Убрать начальные,   конечные  и   между словами должно   быть не больше 1 пробела. ";
+        System.out.println(getStringWithoutSpaces(stringWithSpaces));
+        System.out.println();
+
+
+        //К сожалению 6 задание не осилел...=(
+        File file = new File("\\Users\\mddfs\\IdeaProjects\\JavaElementary\\src\\test.txt");
+        Scanner scanner = new Scanner(file);
+        ArrayList<String> names = new ArrayList<String>();
+
+        while (scanner.hasNextLine()) {
+            names.add(scanner.nextLine());
+        }
+        scanner.close();
+        String[] namesArr = names.toArray(new String[0]);
+
+        char[][] array = new char[namesArr.length][];
+        for (int i = 0; i < namesArr.length; i++) {
+            array[i] = namesArr[i].toCharArray();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int f = 0; f < array[i].length; f++) {
+                System.out.print(array[i][f]);
+
+            }
+            System.out.println();
+        }
+        String word = "test";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                if (array[i][j] == word.charAt(0)) {
+
+
+                }
+
+            }
+        }
+
+    }
+
+
+    //    Метод к 4-тому заданию. Метод, который вернет String в которой будут в одну строку числа от 1 до 30.
+    public static String getString() {
+        String stringOfNumbers = null;
+        String test = null;
+        for (int i = 1; i < 31; i++) {
+            stringOfNumbers = stringOfNumbers + "(" + (test = Integer.toString(i)) + ")";
+        }
+        stringOfNumbers = deleteCharacters(stringOfNumbers, 0, 4);
+
+        return stringOfNumbers;
+    }
+
+    //    Метод к 4-тому заданию. Медот, которые удаляет не нужные элементы строки.
+    public static String deleteCharacters(String str, int from, int to) {
+        return str.substring(0, from) + str.substring(to);
+    }
+
+    //    Метод к 5-тому заданию. Метод, который убирает лишние пробелы в строке и возвращает строку.
+    public static String getStringWithoutSpaces(String stringWithSpaces) {
+        return stringWithSpaces.trim().replaceAll("\\s{2,}", " ");
+    }
+
+}
