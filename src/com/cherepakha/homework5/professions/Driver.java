c8159b8c4d4d451db4779ee33f9d216960647b1b
Ivan Cherepakha <mddfsaff@gmail.com>
2022-10-29 19:36:53 +0300

    Work on mistakes. Adding the PERSON class to the package of the 5th task.

--- src/com/cherepakha/homework5/professions/Driver.java
+++ src/com/cherepakha/homework5/professions/Driver.java
@@ -1,28 +1,16 @@
 package com.cherepakha.homework5.professions;
 
-import com.cherepakha.homework3.Person;
+import com.cherepakha.homework5.Person;
 
 public class Driver extends Person {
-    private String surname;
-    private String name;
-    private String patronymic;
     private int drivingExperience;
 
-    public Driver(String surname, String name, String patronymic) {
-        super(surname, name, patronymic);
-    }
-
-    public Driver(String surnameNamePatronymic) {
-        super(surnameNamePatronymic);
-    }
 
     @Override
     public String toString() {
         return "Driver{" +
-                "surname='" + surname + '\'' +
-                ", name='" + name + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", drivingExperience=" + drivingExperience +
+                "drivingExperience=" + drivingExperience +
+                ", fullName='" + fullName + '\'' +
                 '}';
     }
 }
